25f7dcf0f13e3146a0a8d0d230d22e17ea38b8d8
xideme <soopka@gmail.com>
2024-09-24 14:42:50 +0300

    Google Search add

---
+++ Google Search/UnitTest1.cs
@@ -0,0 +1,45 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+class GoogleSearchWeather
+{
+    static void Main(string[] args)
+    {
+        // Initialize ChromeDriver (Make sure ChromeDriver is installed and added to the PATH)
+        IWebDriver driver = new ChromeDriver();
+
+        try
+        {
+            driver.Navigate().GoToUrl("https://www.google.ee");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            try
+            {
+                // Locate the "Nõustu kõigiga" button by its text and click it
+                IWebElement acceptAllButton = wait.Until(drv => drv.FindElement(By.XPath("//button[.='Nõustu kõigiga']")));
+                acceptAllButton.Click();
+            }
+            catch (WebDriverTimeoutException)
+            {
+                Console.WriteLine("Nõustu kõigiga button not found or took too long to appear.");
+            }
+
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+
+            searchBox.SendKeys("Weather");
+
+            searchBox.Submit();
+
+            System.Threading.Thread.Sleep(3000); // Wait for 3 seconds
+        }
+        finally
+        {
+            // Close the browser
+            driver.Quit();
+        }
+    }
+}
+
